20bb34b0598a32d6bd9161d50cf35e1cf6970c7d
sumit <153519464+Sumit-Chourey@users.noreply.github.com>
2023-12-12 14:40:52 +0530

    first commit

---
+++ src/main/java/com/jsp/test/JspConfigProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jsp.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspConfigProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspConfigProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsp/test/JspConfigProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspConfigProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
